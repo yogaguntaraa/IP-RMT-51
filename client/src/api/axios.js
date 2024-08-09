4b84949779a9d47fac2671117bec54da3c01bbc2
yogaguntaraa <164550595+yogaguntaraa@users.noreply.github.com>
2024-08-09 15:02:50 +0700

    feat:client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:80/"
+  // baseURL: "http://localhost:80/",
+  baseURL: "https://ip-server.yogaguntara.xyz/"
 });
 
 export default instance;
